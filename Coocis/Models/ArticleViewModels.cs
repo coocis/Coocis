d467e101371ec299eb0bbf9dc4bf06aab5f5cf3c
Administrator <Administrator@PC-20160708KLZR>
2016-10-10 14:59:02 +0800

    commit

---
+++ Coocis/Models/ArticleViewModels.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Coocis.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Author { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime ReleaseDateTime { get; set; }
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+    }
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class ArticleDBContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public ArticleDBContext() : base("name=MySqlConnection")
+        {
+            Database.SetInitializer<ArticleDBContext>(new DropCreateDatabaseAlways<ArticleDBContext>());
+        }
+    }
+}
